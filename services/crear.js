c336d28345931a278366a402400b1f8af7713e9e
MarcosGual <marcos.gual@outlook.com>
2022-10-15 13:27:27 -0300

    servicio crear actualizado

--- services/crear.js
+++ services/crear.js
@@ -1,23 +1,20 @@
-const { pick } = require('ramda')
-const db = require('../models/index.js');
-const Mazo=db.models.Mazo;
-const { generarMazo } = require('../utils/mazo')
+const { pick } = require("ramda");
+const db = require("../models/index.js");
+const Mazo = db.models.Mazo;
+const { generarMazo } = require("../utils/mazo");
 
 const crearServicio = async (mazos) => {
   const cards = generarMazo(mazos);
+  const mazo = new Mazo({ cartasDisponibles: cards, cartasRetiradas: [] });
 
-  const mazo = new Mazo({ cartasDisponibles: cards,
-cartasRetiradas: [] })
-  //await mazo.save()
-  console.log(mazo)
   const nuevoMazo = await Mazo.create({
     cartasDisponibles: mazo.cartasDisponibles,
-cartasRetiradas:mazo.cartasRetiradas
-});
+    cartasRetiradas: mazo.cartasRetiradas,
+  });
 
-  const respuesta = pick(['id', 'cartasDisponibles']);
+  const respuesta = pick(["id", "cartasDisponibles"]);
 
   return respuesta(nuevoMazo);
-}
+};
 
-module.exports = {crearServicio};
+module.exports = { crearServicio };
