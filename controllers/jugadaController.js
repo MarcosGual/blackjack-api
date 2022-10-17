d14de6351af3b81f5abcd7ec275ddf39207f6233
MarcosGual <marcos.gual@outlook.com>
2022-10-17 13:51:29 -0300

    actualizar+consultar jugadas

--- controllers/jugadaController.js
+++ controllers/jugadaController.js
@@ -3,6 +3,21 @@ const { crearService } = require("../services/crear");
 const Usuario = db.models.Usuario;
 const Jugada = db.models.Jugada;
 
+const consultaJugada = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    if (id === null) {
+      res.status(501).send("Debe haber un parámetro id");
+    } else {
+      const jugada = await Jugada.findOne({ where: { id: id } });
+      res.status(200).send({ jugada });
+    }
+  } catch (error) {
+    res.status(501).send({ Error: error.message });
+  }
+};
+
 const nuevaJugada = async (req, res) => {
   try {
     const { UsuarioId, cantMazos } = req.body;
@@ -37,4 +52,39 @@ const nuevaJugada = async (req, res) => {
   }
 };
 
-module.exports = { nuevaJugada };
+const actualizarJugada = async (req, res) => {
+  try {
+    const {
+      JugadaId,
+      puntajeCroupier,
+      puntajeUsuario,
+      cartasCroupier,
+      cartasUsuario,
+      gano,
+      terminada,
+    } = req.body;
+
+    const jugada = await Jugada.findOne({ where: { id: JugadaId } });
+
+    if (!jugada) {
+      return res.status(404).json({
+        error: "La jugada no existe",
+      });
+    } else {
+      jugada.puntajeCroupier = puntajeCroupier;
+      jugada.puntajeUsuario = puntajeUsuario;
+      jugada.cartasCroupier = cartasCroupier;
+      jugada.cartasUsuario = cartasUsuario;
+      jugada.gano = gano;
+      jugada.terminada = terminada;
+
+      const nuevaJugada = await jugada.save();
+
+      res.status(200).send(nuevaJugada);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = { nuevaJugada, actualizarJugada, consultaJugada };
